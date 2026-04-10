a055c3e96cf53fe6bccb5573999ecbb7506795b8
Your Name <youremail@example.com>
2026-04-10 12:40:34 +0500

    home pages change

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,44 @@
-import("./Home.css")
-import Property10 from "../assets/Property10.png";
-const Home=()=>{
-    return(
-        <section
-              className="services"
-              style={{
-                backgroundImage: `url(${Property10})`,
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-                backgroundRepeat: "no-repeat",
-              }}
-              >
-              </section>
-    )
-}
-export default Home
+import "./Home.css";
+import homeee from "../assets/homeee.png";
+
+const Home = () => {
+  return (
+    <section className="homeHero-section">
+      
+      {/* Background Image */}
+      <img src={homeee} alt="bg" className="homeHero-bgImg" />
+
+      {/* Overlay Content */}
+      <div className="homeHero-content">
+
+        <p className="homeHero-hello">Hello!</p>
+
+        <h1 className="homeHero-title">
+          I'm <span>Jenny</span>, <br />
+          Product Designer
+        </h1>
+
+        <p className="homeHero-desc">
+          Jenny’s exceptional product design ensure our website’s success.
+          Highly Recommended
+        </p>
+
+        <div className="homeHero-buttons">
+          <button className="homeHero-portfolio">Portfolio ↗</button>
+          <button className="homeHero-hire">Hire me</button>
+        </div>
+
+        <div className="homeHero-experience">
+          ⭐⭐⭐⭐⭐
+          <p>
+            <strong>10 Years</strong><br />
+            Experience
+          </p>
+        </div>
+
+      </div>
+    </section>
+  );
+};
+
+export default Home;
